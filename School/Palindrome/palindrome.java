771aa4c3911cf9ae2532c992cc3b322a956c7420
Harsh Kakaiya <80671616+harsh-kakaiya@users.noreply.github.com>
2023-12-28 20:56:43 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindrome/palindrome.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            String ans = ob.is_palindrome(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String is_palindrome(int n)
+    {
+        // Code here
+        int sum=0, temp=n;
+        while (temp>0){
+            int a = temp%10;
+            sum=sum*10+a;
+            temp/=10;
+        }
+        if(sum==n) return "Yes";
+        else return "No";
+    }
+}
