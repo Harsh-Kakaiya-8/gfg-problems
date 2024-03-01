d47a3303a5861608b9c3a3dc127e6294e1f85913
Harsh Kakaiya <80671616+harsh-kakaiya@users.noreply.github.com>
2024-03-01 21:41:02 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int palindromicPartition(string str)
+    {
+        // code here
+        int n=str.size();
+        
+        vector<int>dp(n+1,0);
+        
+        for(int i=0; i<=n; i++) dp[i]=i-1;
+        for(int i=0; i<n; i++){
+            for(int j=0; i-j>=0 && i+j<n && str[i-j]==str[i+j]; j++)
+                dp[i+j+1]=min(dp[i+j+1], 1+dp[i-j]);
+                
+            for(int j=1; i-j+1>=0 && i+j<n && str[i-j+1]==str[i+j]; j++)
+                dp[i+j+1] = min(dp[i+j+1], 1+dp[i-j+1]);
+                
+        }return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
