9901860a557e46f970d8d54cd2731a091aa76670
Harsh Kakaiya <80671616+harsh-kakaiya@users.noreply.github.com>
2024-05-29 21:23:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Geek and its Game of Coins/geek-and-its-game-of-coins.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findWinner(int n, int x, int y) {
+        // code here
+        vector<int>dp(n+1, 0);
+        for(int i=1; i<=n; ++i){
+            if(i-1 >=0 && dp[i-1]==0)dp[i]=1;
+            if(i-x>=0 && dp[i-x]==0)dp[i]=1;
+            if(i-y>=0 && dp[i-y]==0)dp[i]=1;
+        }
+        return dp[n];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        int y;
+        scanf("%d", &y);
+
+        Solution obj;
+        int res = obj.findWinner(n, x, y);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
